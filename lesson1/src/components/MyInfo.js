9134b030a2f61e5fd51f7f6830a84d8153f544a3
Karina <karinatkachenko9873@gmail.com>
2020-04-20 16:18:38 +0300

    Lesson1
    Intro_to_React

---
+++ lesson1/src/components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MyInfo() {
+    return (
+      <div>
+          <h1>Karina Tkachenko</h1>
+          <p>I`m 21 years old. I`m from Kyiv, Ukraine. 
+              I`m studying at the IT Step Academy and Kyiv National University of Trade and Economics.</p>
+              <p>I want to visit: </p>
+         <ul>
+             <li>Paris, France</li>
+             <li>Rome, Italy</li>
+             <li>Barcelona, Spain</li>
+         </ul>
+      </div>
+    );
+}
+
+export default MyInfo;
---
+++ lesson1/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyInfo from "./components/MyInfo";
+
+ReactDOM.render(
+  <MyInfo/>,
+  document.getElementById('root')
+);
+
+
